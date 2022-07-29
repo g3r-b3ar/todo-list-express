f5175a8582053da0ad708438182d5c82e3808377
cielo-oro <cielo.oro.studios@gmail.com>
2022-07-29 14:25:23 -0500

    Added more comments

--- server.js
+++ server.js
@@ -1,93 +1,110 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
-
+const express = require('express') // enables use of express
+const app = express() // declaring variable for express
+const MongoClient = require('mongodb').MongoClient // enables use of mongodb
+const PORT = 2121 // admin port variable
+require('dotenv').config() // password encryption
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+	dbConnectionStr = process.env.DB_STRING, // variable to store database connection might include login info
+	dbName = 'todo' // database collection name
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // connecting to mongoDB client
+	.then(client => {
+		console.log(`Connected to ${dbName} Database`) // if connection successful console message
+		db = client.db(dbName) // set db variable to the collection name from database
+	})
 
+app.set('view engine', 'ejs') // set view engine as ejs
+app.use(express.static('public')) // makes everything in the public folder static
+app.use(express.urlencoded({ extended: true })) // middleware url body parser
+app.use(express.json()) // enables use of JSON data in express requests
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+app.get('/', async (request, response) => {
+	// async funtion return promise
+	const todoItems = await db.collection('todos').find().toArray() // sets variable to objects from db into an array
+	const itemsLeft = await db
+		.collection('todos')
+		.countDocuments({ completed: false }) // counts how many to do items are not completed yet
+	response
+		.render('index.ejs', { items: todoItems, left: itemsLeft }) // renders the items into html using ejs
+		// db.collection('todos').find().toArray()      // if not for line above would require the following code
+		// .then(data => {
+		//     db.collection('todos').countDocuments({completed: false})
+		//     .then(itemsLeft => {
+		//         response.render('index.ejs', { items: data, left: itemsLeft })
+		//     })
+		// })
+		.catch(error => console.error(error))
 })
 
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
+	// process post request and responses with ejs
+	db.collection('todos')
+		.insertOne({ thing: request.body.todoItem, completed: false }) // parses to do item from request and inserts into the database
+		.then(result => {
+			console.log('Todo Added') // if successful logs todo added
+			response.redirect('/') // refreshes index.html (new item created will populate on todo list)
+		})
+		.catch(error => console.error(error))
 })
 
 app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
-
+	// process put/update request and responses with ejs
+	db.collection('todos')
+		.updateOne(
+			{ thing: request.body.itemFromJS },
+			{
+				// updates the database object with todo set to true
+				$set: {
+					completed: true, // sets completed property to true
+				},
+			},
+			{
+				sort: { _id: -1 }, // sorts by _id property in descending order database
+				upsert: false, // if no todo item is found do not insert
+			}
+		)
+		.then(result => {
+			console.log('Marked Complete') // logs marked complete
+			response.json('Marked Complete') // responds with JSON string
+		})
+		.catch(error => console.error(error))
 })
 
 app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
-
+	// process put/update request and responses with ejs
+	db.collection('todos')
+		.updateOne(
+			{ thing: request.body.itemFromJS },
+			{
+				// updates the database object with todo set to false
+				$set: {
+					completed: false, // sets completed property to false
+				},
+			},
+			{
+				sort: { _id: -1 }, // sorts by _id property in descending order database
+				upsert: false, // if no todo item is found do not insert
+			}
+		)
+		.then(result => {
+			console.log('Marked Complete') // logs marked complete
+			response.json('Marked Complete') // responds with JSON string
+		})
+		.catch(error => console.error(error))
 })
 
 app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
-
+	// process delete request and responses with ejs
+	db.collection('todos')
+		.deleteOne({ thing: request.body.itemFromJS }) // deletes todo item from the database
+		.then(result => {
+			console.log('Todo Deleted')
+			response.json('Todo Deleted')
+		})
+		.catch(error => console.error(error))
 })
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, () => {
+	console.log(`Server running on port ${PORT}`)
 })
